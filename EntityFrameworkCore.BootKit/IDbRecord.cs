0882acf7d349bb49013e75638c611bb76a3691c7
haiping008 <haiping008@gmail.com>
2017-12-17 08:36:01 -0600

    Add a default IDbRecord interface. Upgrade Dynamic Linq extensions to 1.0.8.

---
+++ EntityFrameworkCore.BootKit/IDbRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCore.BootKit
+{
+    /// <summary>
+    /// EF BootKit will generate a concrete table in corresponding database
+    /// </summary>
+    public interface IDbRecord
+    {
+    }
+}
