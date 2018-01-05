c4ca0fbfa319d0cd8c5119e6b48dbe9368bbda93
haiping008 <haiping008@gmail.com>
2018-01-04 19:14:25 -0600

    Update unit test

--- EntityFrameworkCore.BootKit.UnitTest/DatabaseTest.cs
+++ EntityFrameworkCore.BootKit.UnitTest/DatabaseTest.cs
@@ -16,19 +16,19 @@ namespace EntityFrameworkCore.BootKit.UnitTest
         [TestMethod]
         public void TestSqlite()
         {
-            AddRecord(GetDb());
-            GetRecordsByTableName(GetDb());
-            UpdateRecordsByTableName(GetDb());
-            PatchRecord(GetDb());
+            AddRecord();
+            GetRecordsByTableName();
+            UpdateRecordsByTableName();
+            PatchRecord();
         }
 
         [TestMethod]
         public void TestSqlServer()
         {
-            AddRecord(GetDb());
-            GetRecordsByTableName(GetDb());
-            UpdateRecordsByTableName(GetDb());
-            PatchRecord(GetDb());
+            AddRecord();
+            GetRecordsByTableName();
+            UpdateRecordsByTableName();
+            PatchRecord();
         }
 
         private Database GetDb()
@@ -45,8 +45,10 @@ namespace EntityFrameworkCore.BootKit.UnitTest
             return db;
         }
 
-        private void GetRecordsByTableName(Database db)
+        private void GetRecordsByTableName()
         {
+            var db = GetDb();
+
             var table = db.Table("PizzaOrder");
             var pizzaOrder = table.First(x => x.Id == PIZZA_ORDER_ID) as PizzaOrder;
             Assert.IsNotNull(pizzaOrder.Id);
@@ -54,8 +56,10 @@ namespace EntityFrameworkCore.BootKit.UnitTest
 
         public static String PIZZA_ORDER_ID = "7974f8d9-9124-4e24-a906-2e5bb3323e01";
 
-        private void AddRecord(Database db)
+        private void AddRecord()
         {
+            var db = GetDb();
+
             var pizza = new PizzaOrder
             {
                 Id = PIZZA_ORDER_ID,
@@ -78,8 +82,10 @@ namespace EntityFrameworkCore.BootKit.UnitTest
             Assert.IsTrue(order.PizzaTypes.Count == 2);
         }
 
-        private void UpdateRecordsByTableName(Database db)
+        private void UpdateRecordsByTableName()
         {
+            var db = GetDb();
+
             DateTime dt = DateTime.UtcNow;
 
             var table = db.Table("PizzaOrder");
@@ -90,8 +96,10 @@ namespace EntityFrameworkCore.BootKit.UnitTest
             Assert.IsTrue(po.CreatedTime == dt);
         }
 
-        private void PatchRecord(Database db)
+        private void PatchRecord()
         {
+            var db = GetDb();
+
             DateTime dt = DateTime.UtcNow.AddMinutes(-5);
 
             var patch = new DbPatchModel
@@ -102,11 +110,8 @@ namespace EntityFrameworkCore.BootKit.UnitTest
 
             patch.Values.Add("CreatedTime", dt);
 
-            int row = db.DbTran(() =>
-            {
-                db.Patch<IDbRecord>(patch);
-            });
-
+            int row = db.DbTran(() => db.Patch<IDbRecord>(patch));
+            
             var po = db.Table<PizzaOrder>().Find(PIZZA_ORDER_ID);
             Assert.IsTrue(po.CreatedTime.ToString() == dt.ToString());
         }
